707586bb82642fabc1a97da21aee0db0e810ab01
JPreault <jose@preault.com>
2023-12-01 01:30:43 +0100

    feat: probleme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './sass/index.scss';
 import App from './view/App';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <Router basename='adventCalendar'>
+      <Route index element={<App />}/>
+    </Router>
+  </QueryClientProvider>
+</React.StrictMode>);
